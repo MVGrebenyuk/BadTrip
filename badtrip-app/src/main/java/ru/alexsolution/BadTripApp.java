a1d71d683569da637639bfdd9e1e75af02322844
mgrebeniuk <Maxim13parkour@gmail.com>
2022-08-14 22:26:14 +0300

    Initial commit

---
+++ badtrip-app/src/main/java/ru/alexsolution/BadTripApp.java
@@ -0,0 +1,14 @@
+package ru.alexsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadTripApp
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(BadTripApp.class, args);
+    }
+}
