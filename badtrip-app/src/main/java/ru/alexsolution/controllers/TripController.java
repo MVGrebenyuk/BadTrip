66b2afbf5700f6d562176812005edf2f739900e3
mgrebeniuk <Maxim13parkour@gmail.com>
2022-08-15 10:28:13 +0300

    add trip, repository

--- badtrip-app/src/main/java/ru/alexsolution/controllers/TripController.java
+++ badtrip-app/src/main/java/ru/alexsolution/controllers/TripController.java
@@ -2,12 +2,14 @@ package ru.alexsolution.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.alexsolution.entity.Trip;
 import ru.alexsolution.services.TripService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/trip")
@@ -19,6 +21,11 @@ public class TripController {
     @GetMapping
     private List<Trip> getAllTrips(){
         return service.getAllTrips();
+    }
+
+    @GetMapping("/{id}")
+    private Trip getTripById(@PathVariable UUID id){
+        return service.findTripById(id);
     }
 
 }
