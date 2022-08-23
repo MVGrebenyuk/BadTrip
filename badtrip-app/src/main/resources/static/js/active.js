9ff330132bd02342df2368a99f685e9fbbbd1eba
Spacecreater <hirikomu@yandex.ru>
2022-08-23 19:57:32 +0300

    Доработки главной страницы

--- badtrip-app/src/main/resources/static/js/active.js
+++ badtrip-app/src/main/resources/static/js/active.js
@@ -1,135 +1,48 @@
-(function ($) {
-    'use strict';
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
 
-    if ($.fn.owlCarousel) {
-        // :: 1.0 Welcome Post Slider Active Code
-        $(".welcome-post-sliders").owlCarousel({
-            items: 4,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 1500,
-            margin: 10,
-            nav: true,
-            navText: ['', ''],
-            responsive: {
-                320: {
-                    items: 1
-                },
-                576: {
-                    items: 2
-                },
-                992: {
-                    items: 3
-                },
-                1200: {
-                    items: 4
-                }
-            }
-        })
-        // :: 2.0 Instagram Slider Active Code
-        $(".instargram_area").owlCarousel({
-            items: 6,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 800,
-            nav: true,
-            navText: ['', ''],
-            responsive: {
-                320: {
-                    items: 1
-                },
-                480: {
-                    items: 2
-                },
-                576: {
-                    items: 3
-                },
-                768: {
-                    items: 4
-                },
-                992: {
-                    items: 5
-                },
-                1200: {
-                    items: 6
-                }
-            }
-        })
-        // :: 3.0 Related Post Slider Active Code
-        $(".related-post-slider").owlCarousel({
-            items: 3,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 800,
-            nav: true,
-            margin: 30,
-            navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-            responsive: {
-                320: {
-                    items: 1
-                },
-                480: {
-                    items: 2
-                },
-                768: {
-                    items: 3
-                }
-            }
-        })
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
     }
-
-    // :: 4.0 ScrollUp Active JS
-    if ($.fn.scrollUp) {
-        $.scrollUp({
-            scrollSpeed: 1500,
-            scrollText: '<i class="fa fa-arrow-up" aria-hidden="true"></i>'
-        });
+    if (n < 1) {
+        slideIndex = slides.length
     }
-
-    // :: 5.0 CounterUp Active JS
-    if ($.fn.counterUp) {
-        $('.counter').counterUp({
-            delay: 10,
-            time: 2000
-        });
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-
-    // :: 6.0 PreventDefault a Click
-    $("a[href='#']").on('click', function ($) {
-        $.preventDefault();
-    });
-
-    // :: 7.0 Search Form Active Code
-    $(".searchBtn").on('click', function () {
-        $(".search-hidden-form").toggleClass("search-form-open");
-    });
-
-    // :: 8.0 Search Form Active Code
-    $("#pattern-switcher").on('click', function () {
-        $("body").toggleClass("bg-pattern");
-    });
-    $("#patter-close").on('click', function () {
-        $(this).hide("slow");
-        $("#pattern-switcher").addClass("pattern-remove");
-    });
-
-    // :: 9.0 wow Active Code
-    if ($.fn.init) {
-        new WOW().init();
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-
-    // :: 10.0 matchHeight Active JS
-    if ($.fn.matchHeight) {
-        $('.item').matchHeight();
-    }
-
-    var $window = $(window);
-
-    // :: 11.0 Preloader active code
-    $window.on('load', function () {
-        $('#preloader').fadeOut('slow', function () {
-            $(this).remove();
-        });
-    });
-
-})(jQuery);
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+var i = 1;
+  setInterval(function(){
+  currentSlide(i)
+  i++;
+  if(i == 5)
+  {
+   i = 1;
+  }
+},5000)
