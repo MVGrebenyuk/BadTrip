201c6aa97127baaedcdb21006a1554093477ae3b
mgrebeniuk <Maxim13parkour@gmail.com>
2022-08-24 19:20:15 +0300

    configuration

--- badtrip-app/src/main/java/ru/alexsolution/controllers/TripController.java
+++ badtrip-app/src/main/java/ru/alexsolution/controllers/TripController.java
@@ -1,5 +1,7 @@
 package ru.alexsolution.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +17,31 @@ import java.util.UUID;
 @RequestMapping("/api/v1/trip")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "TRIP API", description = "Контроллер для работы с трипами")
 public class TripController {
 
     private final TripService service;
 
     @GetMapping
+    @Operation(summary = "Получить все туры")
     private List<Trip> getAllTrips(){
         return service.getAllTrips();
     }
 
     @GetMapping("/author/{id}")
+    @Operation(summary = "Получить тур по if автора")
     private TripDto findByAuthor(UUID id){
         return service.getTripByAuthor(id);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получить тур по id")
     private Trip getTripById(@PathVariable UUID id){
         return service.findTripById(id);
     }
 
     @PostMapping
+    @Operation(summary = "Создать тур")
     private void createTrip(Principal principal, TripDto createTripDto){
         service.createTrip(principal, createTripDto);
     }
