50566a6cbbf235a15e8eb5f436d230c04b0b57b8
mgrebeniuk <Maxim13parkour@gmail.com>
2022-11-22 11:54:39 +0300

    change delete

--- badtrip-app/src/main/java/ru/alexsolution/repositories/TripRepository.java
+++ badtrip-app/src/main/java/ru/alexsolution/repositories/TripRepository.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.Cascade;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import ru.alexsolution.entity.Trip;
@@ -37,12 +38,15 @@ public interface TripRepository extends JpaRepository<Trip, UUID> , JpaSpecifica
     @Query("select max(t.duration) from trip t")
     Integer findMaxDuration();
 
+    @Modifying
     @Query(value = "DELETE FROM purchased_to_user where trip_id in(?1);", nativeQuery = true)
     void deleteFromPurchasedById(UUID id);
 
+    @Modifying
     @Query(value = "DELETE FROM favorites_to_user where trip_id in(?1);", nativeQuery = true)
     void deleteFromFavoriteById(UUID id);
 
+    @Modifying
     @Query(value = "DELETE FROM trip where id in(?1)", nativeQuery = true)
     void deleteById(UUID id);
 
